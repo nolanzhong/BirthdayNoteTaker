14c333fef0962334edd5ab077924cc72ecc7e58e
nolanzhong <nolan.zhong@duke.edu>
2022-12-31 13:17:57 -0500

    added save button

--- BirthdayNoteTaker/ViewController.swift
+++ BirthdayNoteTaker/ViewController.swift
@@ -23,9 +23,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        
+        // Casting as? vs. as!
+        if let newName = storedName as? String {
+            nameLabel.text = newName
+        }
+        
+        if let newBirthday = storedBirthday as? String {
+            birthdaylabel.text = newBirthday
+        }
     }
 
     @IBAction func saveClicked(_ sender: Any) {
+        
+        // saving data to database
+        UserDefaults.standard.set(nameTextField.text!, forKey: "name")
+        UserDefaults.standard.set(birthdayTextField.text!, forKey: "birthday")
+        
+        
         nameLabel.text = "Name: \(nameTextField.text!)"
         birthdaylabel.text = "Birthday: \(birthdayTextField.text!)"
     }
