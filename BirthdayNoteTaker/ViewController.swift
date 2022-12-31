0ff6b1dcbd4ee0c7bc4e99ae97214fada4113890
nolanzhong <nolan.zhong@duke.edu>
2022-12-30 22:22:10 -0500

    Initial Commit

---
+++ BirthdayNoteTaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirthdayNoteTaker
+//
+//  Created by Nolan Zhong on 12/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
